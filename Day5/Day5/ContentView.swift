e9c8dcfda92b1d6feeb377ad0e3511484097c239
youten410 <techregisteremail@gmail.com>
2024-04-20 15:30:41 +0900

    add : Day5

---
+++ Day5/Day5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day5
+//
+//  Created by Shun Sato on 2024/04/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day5/Day5/Day5App.swift
@@ -0,0 +1,17 @@
+//
+//  Day5App.swift
+//  Day5
+//
+//  Created by Shun Sato on 2024/04/20.
+//
+
+import SwiftUI
+
+@main
+struct Day5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
