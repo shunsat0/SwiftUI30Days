960d0229827b677e8b51008b1e9cc7a0f5033633
youten410 <techregisteremail@gmail.com>
2024-04-24 01:39:41 +0900

    add : リストの中身を変更

--- Day7/Day7/ContentView.swift
+++ Day7/Day7/ContentView.swift
@@ -8,20 +8,24 @@
 import SwiftUI
 
 struct Character: Hashable {
-    var name:String
+    var name: String
 }
 
 struct ContentView: View {
     
-    @State var isEditing:Bool = true
-    
     @State var characters = [
-        Character(name:"Arya Stark"),
-        Character(name: "Jon Snow"),
-        Character(name: "Jamie Lannister"),
-        Character(name: "Ed Stark"),
-        Character(name: "Taro Yamada"),
-        Character(name: "Shohei Otani")
+        Character(name: "吉田 正尚"),
+        Character(name: "菊池 雄星"),
+        Character(name: "前田 健太"),
+        Character(name: "藤浪 晋太郎"),
+        Character(name: "千賀 滉大"),
+        Character(name: "今永 昇太"),
+        Character(name: "鈴木 誠也"),
+        Character(name: "ラーズ・ヌートバー"),
+        Character(name: "大谷 翔平"),
+        Character(name: "山本 由伸"),
+        Character(name: "松井 裕樹"),
+        Character(name: "ダルビッシュ 有")
     ]
     
     func move(from source: IndexSet,to destination: Int){
@@ -36,12 +40,10 @@ struct ContentView: View {
         NavigationStack {
             List {
                 ForEach(characters, id: \.self) { character in
-                    Text(character.name)
+                    Text("\(character.name)")
                 }
                 .onMove(perform: move)
                 .onDelete(perform: deleteIem)
-                .deleteDisabled(false)
-                .moveDisabled(false)
             }
             .navigationBarItems(trailing: EditButton())
             .navigationTitle("Member")
