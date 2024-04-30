be7fe5872e88b8829f9ee7feb1408dd1172c8aad
youten410 <techregisteremail@gmail.com>
2024-04-30 23:48:20 +0900

    add : Day16

---
+++ Day16/Day16/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Day16
+//
+//  Created by Shun Sato on 2024/04/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @GestureState var dragOffset = CGSize.zero
+    @State private var position = CGSize.zero
+    
+    var body: some View {
+        Image(systemName: "cursorarrow.rays")
+            .font(.system(size: 100))
+            .offset(x: position.width + dragOffset.width, y: position.height + dragOffset.height)
+            .animation(.easeInOut, value: dragOffset)
+            .foregroundColor(.blue)
+            .gesture(
+                DragGesture()
+                    .updating($dragOffset, body: { (value, state, transaction) in
+
+                        state = value.translation
+                    })
+                    .onEnded({ (value) in
+                        self.position.height += value.translation.height
+                        self.position.width += value.translation.width
+                    })
+            )
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day16/Day16/Day16App.swift
@@ -0,0 +1,17 @@
+//
+//  Day16App.swift
+//  Day16
+//
+//  Created by Shun Sato on 2024/04/30.
+//
+
+import SwiftUI
+
+@main
+struct Day16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
